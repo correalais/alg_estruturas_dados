d5090bdfa71783881544024eea850f704b0437b5
Lais Correa <laiscorreamartins@gmail.com>
2022-04-14 16:40:09 -0300

    :art: Adiciona nova funcionalidade de inserir produto no sistema

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Produto import Produto
 from Categoria import Categoria
 
-categoria = Categoria('Alvejante', 25)
-produto = Produto('01', 'Sabão em pó', 10.90, 5.0)
+adicionaProduto = input('Insira o nome do produto que deseja adicionar: ')
+adicionaCod = input('Insira o código do produto que deseja adicionar: ')
+adicionaPreco = float(input('Insira o preço do produto que deseja adicionar: '))
+adicionarQnt = int(input('Insira a quantidade de produto para adicionar: '))
 
-produto.setCategoria(categoria)
 
-produto.cadastrar()
+produto = Produto(adicionaCod, adicionaProduto, adicionaPreco, adicionarQnt)
